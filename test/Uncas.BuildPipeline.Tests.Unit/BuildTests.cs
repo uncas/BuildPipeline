4b522b5cdd16d6a3e0608cbc8366f5e18d277d6f
Ole Lynge Soerensen <olelynge@gmail.com>
2010-12-29 14:30:11 +0000

    Added unit test.
    git-svn-id: http://localhost:81/svn/dev/projects/Uncas.BuildPipeline/trunk@949 90d2b0b2-0e11-ba47-89f1-85e69862abc4

---
+++ test/Uncas.BuildPipeline.Tests.Unit/BuildTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Uncas.BuildPipeline.Tests.Unit
+{
+    using NUnit.Framework;
+    using Uncas.BuildPipeline.Web.Models;
+
+    [TestFixture]
+    public class BuildTests
+    {
+        [Test]
+        public void Construct()
+        {
+            var build = new Build();
+        }
+    }
+}
