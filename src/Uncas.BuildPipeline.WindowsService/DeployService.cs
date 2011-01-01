e91241fc03c75de72a07c79bdb352e79f85ada46
Ole Lynge Soerensen <olelynge@gmail.com>
2011-01-01 13:34:12 +0000

    Adding windows service skeleton.
    git-svn-id: http://localhost:81/svn/dev/projects/Uncas.BuildPipeline/trunk@1010 90d2b0b2-0e11-ba47-89f1-85e69862abc4

---
+++ src/Uncas.BuildPipeline.WindowsService/DeployService.cs
@@ -0,0 +1,33 @@
+﻿namespace Uncas.BuildPipeline.WindowsService
+{
+    using System.ServiceProcess;
+    using System.Timers;
+
+    public partial class DeployService : ServiceBase
+    {
+        private readonly Timer timer;
+
+        public DeployService()
+        {
+            InitializeComponent();
+            const int intervalSeconds = 10;
+            this.timer = new Timer(intervalSeconds * 1000);
+            this.timer.Elapsed += 
+                new ElapsedEventHandler(timer_Elapsed);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            this.timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            this.timer.Stop();
+        }
+
+        private void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+        }
+    }
+}
---
+++ src/Uncas.BuildPipeline.WindowsService/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Uncas.BuildPipeline.WindowsService
+{
+    using System.ServiceProcess;
+
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        public static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+			{ 
+				new DeployService() 
+			};
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
