bceb78e9861983c498178a5d1a5ac6f27e6579c0
Ole Lynge Soerensen <olelynge@gmail.com>
2011-01-01 11:27:51 +0000

    Added test class for deployment repository.
    git-svn-id: http://localhost:81/svn/dev/projects/Uncas.BuildPipeline/trunk@1003 90d2b0b2-0e11-ba47-89f1-85e69862abc4

---
+++ test/Uncas.BuildPipeline.Tests.Integration/Repositories/DeploymentRepositoryTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Uncas.BuildPipeline.Tests.Integration.Repositories
+{
+    using System;
+    using System.Linq;
+    using NUnit.Framework;
+    using Uncas.BuildPipeline.Models;
+    using Uncas.BuildPipeline.Repositories;
+
+    [TestFixture]
+    public class DeploymentRepositoryTests
+    {
+        private IDeploymentRepository deploymentRepository;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            this.deploymentRepository = new DeploymentRepository();
+        }
+
+        [Test]
+        [Ignore]
+        public void AddDeployment_WhenAdded_CanBeRetrieved()
+        {
+            const int pipelineId=1;
+            const int environmentId=1;
+            DateTime scheduledStart = DateTime.Now.AddMinutes(3d);
+            var deployment = new Deployment(
+                pipelineId,
+                environmentId,
+                scheduledStart);
+            this.deploymentRepository.AddDeployment(deployment);
+
+            var result = 
+                this.deploymentRepository.GetDeployments(pipelineId);
+
+            Assert.AreEqual(1, result.Count());
+        }
+
+        // TODO: Test that when a deployment has been deployed, it should not be deployed again....
+    }
+}
