2c17e875e5d46f60e23759b5157d5e25068d9269
Ole Lynge Soerensen <olelynge@gmail.com>
2011-08-04 13:02:37 +0000

    Generalizing.
    git-svn-id: http://localhost:81/svn/dev/projects/Uncas.BuildPipeline/trunk@1473 90d2b0b2-0e11-ba47-89f1-85e69862abc4

--- src/Uncas.BuildPipeline.Web/Bootstrapper.cs
+++ src/Uncas.BuildPipeline.Web/Bootstrapper.cs
@@ -1,31 +1,50 @@
 ﻿namespace Uncas.BuildPipeline.Web
 {
+    using System;
+    using System.Collections.Generic;
     using Uncas.BuildPipeline.ApplicationServices;
     using Uncas.BuildPipeline.Repositories;
     using Uncas.BuildPipeline.Utilities;
 
     public static class Bootstrapper
     {
+        private static Dictionary<Type, Func<object>> _registered;
+
+        private static Dictionary<Type, Func<object>> Registered
+        {
+            get
+            {
+                if (_registered == null)
+                {
+                    _registered = new Dictionary<Type, Func<object>>();
+                    _registered.Add(
+                        typeof(IDeploymentService),
+                        () => new DeploymentService(
+                            Resolve<IEnvironmentRepository>(),
+                            Resolve<IPipelineRepository>(),
+                            new DeploymentRepository(),
+                            new DeploymentUtility()));
+                    _registered.Add(
+                        typeof(IEnvironmentRepository),
+                        () => new EnvironmentRepository());
+                    _registered.Add(
+                        typeof(IPipelineRepository),
+                        () => new PipelineRepository());
+                }
+
+                return _registered;
+            }
+        }
+
         public static T Resolve<T>() where T : class
         {
-            if (typeof(T) == typeof(IDeploymentService))
+            Type interfaceType = typeof(T);
+            if (!Registered.ContainsKey(interfaceType))
             {
-                return new DeploymentService(
-                    Resolve<IEnvironmentRepository>(),
-                    Resolve<IPipelineRepository>(),
-                    new DeploymentRepository(),
-                    new DeploymentUtility()) as T;
-            }
-            else if (typeof(T) == typeof(IEnvironmentRepository))
-            {
-                return new EnvironmentRepository() as T;
-            }
-            else if (typeof(T) == typeof(IPipelineRepository))
-            {
-                return new PipelineRepository() as T;
+                return null;
             }
 
-            return null;
+            return Registered[interfaceType]() as T;
         }
     }
 }
