9b2259bb993ff197a9791f769b2c2c4cd72e33f3
Ole Lynge Soerensen <olelynge@gmail.com>
2011-08-05 18:32:52 +0000

    Restyled with ReSharper.
    git-svn-id: http://localhost:81/svn/dev/projects/Uncas.BuildPipeline/trunk@1474 90d2b0b2-0e11-ba47-89f1-85e69862abc4

--- src/Uncas.BuildPipeline.Web/Controllers/HomeController.cs
+++ src/Uncas.BuildPipeline.Web/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿namespace Uncas.BuildPipeline.Web.Controllers
 {
+    using System.Collections.Generic;
     using System.Linq;
     using System.Web.Mvc;
     using Uncas.BuildPipeline.ApplicationServices;
+    using Uncas.BuildPipeline.Models;
     using Uncas.BuildPipeline.Repositories;
     using Uncas.BuildPipeline.Web.Mappers;
     using Uncas.BuildPipeline.Web.ViewModels;
@@ -10,61 +12,60 @@
     public class HomeController : BaseController
     {
         private const int BuildPageSize = 10;
-
-        private readonly IEnvironmentRepository environmentRepository;
-        private readonly IPipelineRepository pipelineRepository;
-        private readonly IDeploymentService deploymentService;
+        private readonly IDeploymentService _deploymentService;
+        private readonly IEnvironmentRepository _environmentRepository;
+        private readonly IPipelineRepository _pipelineRepository;
 
         public HomeController()
         {
-            this.environmentRepository = Bootstrapper.Resolve<IEnvironmentRepository>();
-            this.deploymentService = Bootstrapper.Resolve<IDeploymentService>();
-            this.pipelineRepository = Bootstrapper.Resolve<IPipelineRepository>();
+            _environmentRepository = Bootstrapper.Resolve<IEnvironmentRepository>();
+            _deploymentService = Bootstrapper.Resolve<IDeploymentService>();
+            _pipelineRepository = Bootstrapper.Resolve<IPipelineRepository>();
         }
 
         [HttpGet]
         public ActionResult Index()
         {
-            var pipelines = this.pipelineRepository.GetPipelines(BuildPageSize);
-            var viewModel = PipelineMapper.MapToPipelineIndexViewModel(pipelines);
+            IEnumerable<Pipeline> pipelines = _pipelineRepository.GetPipelines(BuildPageSize);
+            PipelineIndexViewModel viewModel = PipelineMapper.MapToPipelineIndexViewModel(pipelines);
             return View(viewModel);
         }
 
         [HttpGet]
         public ActionResult Deploy(int pipelineId)
         {
-            var environments = this.environmentRepository.GetEnvironments();
-            var pipeline = this.pipelineRepository.GetPipeline(pipelineId);
-            var deployments = this.deploymentService.GetDeployments(pipelineId);
-            var environmentViewModels = environments.Select(
+            IEnumerable<Environment> environments = _environmentRepository.GetEnvironments();
+            Pipeline pipeline = _pipelineRepository.GetPipeline(pipelineId);
+            IEnumerable<Deployment> deployments = _deploymentService.GetDeployments(pipelineId);
+            IEnumerable<EnvironmentViewModel> environmentViewModels = environments.Select(
                 e => new EnvironmentViewModel
-            {
-                EnvironmentId = e.Id,
-                EnvironmentName = e.EnvironmentName
-            });
-            var pipelineViewModel = PipelineMapper.MapToPipelineViewModel(pipeline);
+                         {
+                             EnvironmentId = e.Id,
+                             EnvironmentName = e.EnvironmentName
+                         });
+            PipelineViewModel pipelineViewModel = PipelineMapper.MapToPipelineViewModel(pipeline);
             var deployViewModel = new DeployViewModel
-            {
-                Environments = environmentViewModels,
-                Deployments = deployments.Select(
-                    d => new DeploymentViewModel
-                    {
-                        Created = d.Created,
-                        Started = d.Started,
-                        Completed = d.Completed,
-                        EnvironmentName = environments.FirstOrDefault(
-                            e => e.Id == d.EnvironmentId).EnvironmentName
-                    }),
-                Pipeline = pipelineViewModel
-            };
+                                      {
+                                          Environments = environmentViewModels,
+                                          Deployments = deployments.Select(
+                                              d => new DeploymentViewModel
+                                                       {
+                                                           Created = d.Created,
+                                                           Started = d.Started,
+                                                           Completed = d.Completed,
+                                                           EnvironmentName = environments.FirstOrDefault(
+                                                               e => e.Id == d.EnvironmentId).EnvironmentName
+                                                       }),
+                                          Pipeline = pipelineViewModel
+                                      };
             return View(deployViewModel);
         }
 
         [HttpPost]
         public ActionResult Deploy(int environmentId, int pipelineId)
         {
-            this.deploymentService.ScheduleDeployment(pipelineId, environmentId);
-            return RedirectToAction("Deploy", new { PipelineId = pipelineId });
+            _deploymentService.ScheduleDeployment(pipelineId, environmentId);
+            return RedirectToAction("Deploy", new {PipelineId = pipelineId});
         }
 
         [HttpGet]
