809ab6273f8be2be2b65532f3e8d8280240a34e4
Ole Lynge Soerensen <olelynge@gmail.com>
2011-08-11 10:09:47 +0000

    Changing to use program runner in core.
    git-svn-id: http://localhost:81/svn/dev/projects/Uncas.BuildPipeline/trunk@1496 90d2b0b2-0e11-ba47-89f1-85e69862abc4

--- src/Uncas.BuildPipeline.WindowsService/Program.cs
+++ src/Uncas.BuildPipeline.WindowsService/Program.cs
@@ -1,9 +1,6 @@
 ﻿namespace Uncas.BuildPipeline.WindowsService
 {
     using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Reflection;
     using System.ServiceProcess;
     using Uncas.BuildPipeline.ApplicationServices;
     using Uncas.Core.Services;
@@ -15,105 +12,22 @@
         /// </summary>
         private const string ServiceName = "DeployService";
 
-        /// <summary>
-        /// Mapping of console command line args to ServiceManagerCommands.
-        /// </summary>
-        private static readonly Dictionary<string, ServiceManagerCommand> _commands =
-            new Dictionary<string, ServiceManagerCommand>
-                {
-                    { "-console", ServiceManagerCommand.Application },
-                    { "-install", ServiceManagerCommand.Install },
-                    { "-uninstall", ServiceManagerCommand.Uninstall },
-                    { "-start", ServiceManagerCommand.Start },
-                    { "-stop", ServiceManagerCommand.Stop }
-                };
+        private static readonly Func<ServiceBase> GetServiceToRun =
+            () => new DeployService();
+
+        private static readonly Action ActionToRun =
+            () => Bootstrapper.Resolve<IDeploymentService>().DeployDueDeployments();
 
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         public static void Main(string[] args)
         {
-            try
-            {
-                if (Debugger.IsAttached)
-                {
-                    Debugger.Break();
-                }
-
-                if (args.Length == 0)
-                {
-                    ServiceMain();
-                    return;
-                }
-
-                ServiceManagerCommand command;
-                if (!TryParseCommandLine(args, out command))
-                {
-                    PrintUsage();
-                    return;
-                }
-
-                var serviceManager = new ServiceManager(ServiceName);
-                if (command == ServiceManagerCommand.Application)
-                {
-                    const string message = @"Running in console mode.";
-                    Console.WriteLine(message);
-                    Bootstrapper.Resolve<IDeploymentService>().DeployDueDeployments();
-                    Console.Read();
-                }
-                else
-                {
-                    serviceManager.RunCommand(command);
-                }
-            }
-            catch (Exception ex)
-            {
-                EventLog.WriteEntry(
-                    "DeployService",
-                    ex.ToString(),
-                    EventLogEntryType.Error);
-            }
-        }
-
-        private static void PrintUsage()
-        {
-            string exeName = Assembly.GetExecutingAssembly().ManifestModule.Name;
-            Console.WriteLine("Usage:");
-            foreach (var item in _commands)
-            {
-                Console.WriteLine(@"  " + exeName + @" " + item.Key);
-            }
-
-            Console.Read();
-        }
-
-        private static void ServiceMain()
-        {
-            var ServicesToRun = new ServiceBase[]
-                                    {
-                                        new DeployService()
-                                    };
-            ServiceBase.Run(ServicesToRun);
-        }
-
-        private static bool TryParseCommandLine(
-            string[] args,
-            out ServiceManagerCommand command)
-        {
-            command = ServiceManagerCommand.Unknown;
-            if (args.Length > 1)
-            {
-                return false;
-            }
-
-            string commandLineArg = args[0];
-            if (_commands.ContainsKey(commandLineArg))
-            {
-                command = _commands[commandLineArg];
-                return true;
-            }
-
-            return false;
+            new ProgramRunner().RunProgram(
+                args,
+                ActionToRun,
+                ServiceName,
+                GetServiceToRun);
         }
     }
 }
