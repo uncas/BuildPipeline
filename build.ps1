50db6635373295300a6bc4441e2f49ffc7010f99
Ole Lynge Soerensen <olelynge@gmail.com>
2013-03-24 08:02:11 +0100

    Add powershell build script.

---
+++ build.ps1
@@ -0,0 +1,215 @@
+﻿param (
+    [string]$task = "Compile",
+    [string]$branch = "master"
+)
+
+$baseDir  = Resolve-Path .
+$srcDir = "$baseDir\src"
+$testDir = "$baseDir\test"
+$outputDir = "$baseDir\output"
+$collectDir = "$outputDir\collect"
+
+. "$baseDir\build_ext.ps1"
+
+$versionMajor = 1
+$versionMinor = 0
+$versionBuild = 0
+$year = (Get-Date).year
+
+$configuration = "Release"
+
+$serviceName = "Uncas.BuildPipeline.WindowsService"
+
+$solutionFile = "$baseDir\Uncas.BuildPipeline.sln"
+$nunitFolder = "$baseDir\packages\NUnit.Runners.2.6.0.12051\tools"
+$nunitExe = "$nunitFolder\nunit-console.exe"
+
+function Clean {
+    Clean-Folder $collectDir
+    Clean-Folder $outputDir
+}
+
+function Init {
+    Clean
+    
+    if (!(Test-Path $outputDir))
+    {
+        mkdir $outputDir
+    }
+    if (!(Test-Path $collectDir))
+    {
+        mkdir $collectDir
+    }
+
+    Generate-Assembly-Info `
+        -file "$baseDir\src\VersionInfo.cs" `
+        -company "Uncas" `
+        -product "Uncas.BuildPipeline" `
+        -version "$versionMajor.$versionMinor.$versionBuild" `
+        -copyright "Copyright (c) $year, Uncas"
+}
+
+function ThrowIfErrorOnLastExecution {
+    if ($LASTEXITCODE -ne 0) {
+        throw "Error - last exit code was $LASTEXITCODE!"
+    }
+}
+
+function Compile {
+    Init
+    msbuild $solutionFile /p:Configuration=$configuration /p:RunCodeAnalysis=false /t:Rebuild
+    ThrowIfErrorOnLastExecution
+}
+
+function UnitTest {
+    Compile
+    Run-Test "Uncas.BuildPipeline.Tests.Unit" $outputDir
+}
+
+function UpdateDb {
+    UnitTest
+}
+
+function IntegrationTest {
+    UpdateDb
+    Run-Test "Uncas.BuildPipeline.Tests.Integration" $outputDir
+}
+
+function Collect {
+    IntegrationTest
+    
+    Copy-WebApplication $srcDir "Uncas.BuildPipeline" $collectDir
+    copy $srcDir\Uncas.BuildPipeline.WindowsService\bin\$configuration $collectDir\Uncas.BuildPipeline.WindowsService -recurse
+}
+
+function FlipWebSite($siteName, $physicalPath) {
+    $session = New-PSSession -ComputerName $deployMachine
+    if (!$session) { return }
+    $block = { 
+        param($siteName, $physicalPath)
+        if (!$physicalPath -or !$siteName) { return }
+        Add-PSSnapin WebAdministration
+        Set-ItemProperty "IIS:\Sites\$siteName" -name physicalPath -value $physicalPath
+    }
+    Invoke-Command -Session $session -ScriptBlock $block -argumentlist $siteName, $physicalPath
+}
+
+function GetAndStartStopwatch {
+    return [System.Diagnostics.Stopwatch]::StartNew()
+}
+
+function StopAndWriteStopwatch ($stopwatch, $description) {
+    $stopwatch.Stop()
+    $totalSeconds = $stopwatch.Elapsed.TotalSeconds
+    Write-Host "$totalSeconds seconds: $description"
+}
+
+function DeployService {
+    if ($branch -ne "master") { return }
+
+    # Stopping service:
+    $sw = GetAndStartStopwatch
+    $service = Get-Service -ComputerName $deployMachine -Name $serviceName
+    if ($service.Status -ne "Stopped") {
+        $service.Stop()
+    }
+    StopAndWriteStopwatch $sw "Stop service"
+
+    # Waiting for the service to be completely shut down:
+    $sw = GetAndStartStopwatch
+    $waited = 0
+    $waitInterval = 1
+    $waitLimit = 120
+    while ($service.Status -ne "Stopped") {
+        Start-Sleep -s $waitInterval
+        $waited += $waitInterval
+        if ($waited -gt $waitLimit) { break }
+        $service = Get-Service -ComputerName $deployMachine -Name $serviceName
+    }
+    # Extra time to wait, such that files are not in use anymore:
+    Start-Sleep -s 5
+    StopAndWriteStopwatch $sw "Wait for service shut-down"
+
+    # Deploying files to service:
+    $sw = GetAndStartStopwatch
+    Sync-Folders "$collectDir\$serviceName" $remoteServiceFolder
+    StopAndWriteStopwatch $sw "Deploy files to service"
+
+    # Starting service:
+    $sw = GetAndStartStopwatch
+    $service.Start()
+    StopAndWriteStopwatch $sw "Start service"
+}
+
+function DeployWeb {
+    $webRootName = "deliverypipeline"
+    $siteName = "DeliveryPipeline"
+    if ($branch -ne "master") {
+        $webRootName = "deliverypipeline-$branch"
+        $siteName = "DeliveryPipeline-$branch"
+    }
+    $relativeWebRoot = "inetpub\wwwroot\$webRootName"
+    $remoteWebRoot = "\\$deployMachine\c$\$relativeWebRoot"
+
+    $sha = (git rev-parse HEAD)
+    $webFolder = "$sha"
+    $localWebFolder = "C:\$relativeWebRoot\$webFolder"
+    $remoteWebFolder = "$remoteWebRoot\$webFolder"
+
+    # Deploying files to website:
+    $sw = GetAndStartStopwatch
+    if (!(Test-Path $remoteWebRoot)) { mkdir $remoteWebRoot }
+    $previousWebsiteVersions = (gci $remoteWebRoot)
+    Sync-Folders "$collectDir\Uncas.BuildPipeline" $remoteWebFolder
+    StopAndWriteStopwatch $sw "Deploy files to website"
+
+    # TODO: Consider warming the new location up?
+
+    # Flipping website:
+    $sw = GetAndStartStopwatch
+    FlipWebSite $siteName $localWebFolder
+    StopAndWriteStopwatch $sw "Flip website"
+
+    # Removing old versions of website:
+    $sw = GetAndStartStopwatch
+    foreach ($item in $previousWebsiteVersions) {
+        if ($item.Mode -eq "d----") {
+            $fullName = $item.FullName
+            # Do not remove current version, relevant if we're re-deploying same version as last time:
+            if ($fullName.Contains($webFolder)) { continue }
+            Write-Host "Removing $fullName"
+            rmdir $fullName -force -recurse
+        }
+    }
+    StopAndWriteStopwatch $sw "Remove old versions of website"
+}
+
+function Deploy {
+    Collect
+    DeployService
+    DeployWeb
+}
+
+if ($task -and (Test-Path "Function:\$task")) {
+    Write-Host "Invoking the task '$task' for branch '$branch'."
+    Invoke-Expression "$task"
+}
+else {
+    Write-Host "The task '$task' was not found."
+}
+
+if ($LASTEXITCODE -ne 0) {
+    throw "Build error - last exit code was $LASTEXITCODE!"
+}
+
+# Notes on getting powershell IIS remoting to work:
+
+# On the client (build server):
+# - List trusted hosts: Get-Item WSMan:\localhost\Client\TrustedHosts
+# - Use server name, not IP.
+# - Configure trusted hosts: winrm set winrm/config/client '@{TrustedHosts="my-server"}'
+
+# On the host (web server):
+# - Install IIS WebAdministration powershell snapin, can be done with Web Platform Installer
+# - Check if there is a listener: Get-ChildItem WSMan:\localhost\Listener
+# - Add listener: Enable-PSRemoting –force
---
+++ build_ext.ps1
@@ -0,0 +1,129 @@
+﻿# Original from https://github.com/ayende/rhino-mocks/blob/master/psake_ext.ps1
+
+function Clean-Folder {
+    param(
+        [string]$folder = $(throw "folder is required")
+    )
+    if (Test-Path $folder)
+    {
+        rmdir -force -recurse $folder
+    }
+}
+
+function Remove-Folder-Contents {
+    param (
+        [string]$folder = $(throw "folder is required")
+    )
+
+    if (Test-Path $folder)
+    {
+        Remove-Item -Path $folder\* -recurse -force
+    }
+    else
+    {
+        mkdir $folder
+    }
+}
+
+function Sync-Folders
+{
+    param (
+        [string]$source = $(throw "source is required"),
+        [string]$destination = $(throw "destination is required")
+    )
+
+    Remove-Folder-Contents $destination
+    copy $source\* $destination -recurse
+}
+
+function Copy-WebApplication {
+    param (
+        [string]$sourceParentFolder = $(throw "source parent folder is required"),
+        [string]$webApplicationName = $(throw "web application name is required"),
+        [string]$destinationParentFolder = $(throw "destination parent folder is required")
+     )
+
+     mkdir "$collectDir\$webApplicationName"
+
+    $webProjectFile = "$sourceParentFolder\$webApplicationName\$webApplicationName.csproj"
+    msbuild $webProjectFile /p:"Configuration=$configuration;PipelineDependsOnBuild=True;RunCodeAnalysis=false" /p:WebProjectOutputDir=$collectDir\$webApplicationName\ /p:OutDir=$destinationParentFolder\$webApplicationName\bin\ /t:Rebuild /t:ResolveReferences /t:_WPPCopyWebApplication
+}
+
+function Get-Git-Commit
+{
+    $git = "C:\Program Files (x86)\git\cmd\git.cmd"
+    $gitLog = & "$git" log --oneline -1
+    return $gitLog.Split(' ')[0]
+}
+
+function Get-Git-CommitCount
+{
+    $git = "C:\Program Files (x86)\git\cmd\git.cmd"
+    $gitLog = & "$git" log --pretty=oneline
+    return $gitLog.length
+}
+
+function Generate-Assembly-Info
+{
+param(
+	[string]$company, 
+	[string]$product, 
+	[string]$copyright, 
+	[string]$version,
+	[string]$file = $(throw "file is a required parameter.")
+)
+  $commit = Get-Git-Commit
+  $commitCount = Get-Git-CommitCount
+  $fullVersion = "$version.$commitCount"
+  $script:fullVersion = $fullVersion
+  "Version $fullVersion (commit hash: $commit, commit log count: $commitCount)"
+  $asmInfo = "using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompanyAttribute(""$company"")]
+[assembly: AssemblyProductAttribute(""$product"")]
+[assembly: AssemblyCopyrightAttribute(""$copyright"")]
+[assembly: AssemblyVersionAttribute(""$fullVersion"")]
+[assembly: AssemblyInformationalVersionAttribute(""$fullVersion-$commit"")]
+[assembly: AssemblyFileVersionAttribute(""$fullVersion"")]
+[assembly: AssemblyDelaySignAttribute(false)]
+"
+
+	$dir = [System.IO.Path]::GetDirectoryName($file)
+	if ([System.IO.Directory]::Exists($dir) -eq $false)
+	{
+		Write-Host "Creating directory $dir"
+		[System.IO.Directory]::CreateDirectory($dir)
+	}
+	Write-Host "Generating assembly info file: $file"
+	out-file -filePath $file -encoding UTF8 -inputObject $asmInfo
+}
+
+function Run-Test
+{
+    param(
+        [string]$testProjectName = $(throw "file is a required parameter."),
+        [string]$outDir = $(throw "out dir is a required parameter.")
+    )
+
+    $testResultFile = "$outDir\$testProjectName.TestResult.xml"
+    & $nunitExe "$testDir\$testProjectName\bin\$configuration\$testProjectName.dll" /xml=$testResultFile
+
+    if ($lastExitCode -ne 0) {
+        throw "One or more failures in tests - see details above."
+    }
+}
+
+#function Replace-File
+#{
+#    param {
+#        [string]$sourceFile = $(throw "source file is required"),
+#        [string]$targetFile = $(throw "target file is required"),
+#        [string]$originalValue = $(throw "original value is required"),
+#        [string]$finalValue = $(throw "final value is required")
+#    }
+#
+#    (cat $sourceFile) -replace '$originalValue', "$finalValue" > $targetFile
+#}
