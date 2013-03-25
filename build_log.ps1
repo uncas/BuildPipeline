99fad299654c3679fc85ab5f685e0d4af4daf01f
Ole Lynge Soerensen <olelynge@gmail.com>
2013-03-25 20:21:41 +0100

    Log package: Pseudo code script.

---
+++ build_log.ps1
@@ -0,0 +1,11 @@
+﻿function LogPackage (
+        $projectName = "BuildPipeline",
+        $branchName = "master",
+        $revision = (git rev-parse HEAD),
+        $stepName = "Package",
+        $packagePath) {
+    Write-Host "Logging start..."
+    $baseApiUrl = "http:blabla"
+    CallEndPoint $projectName $branchName $revision $stepName $packagePath
+}
+    
